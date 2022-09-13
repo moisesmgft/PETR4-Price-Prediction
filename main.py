38756976bddfb3382f8dcf9835f627df0cbbb9b5
moisesmgft <87101637+moisesmgft@users.noreply.github.com>
2022-09-13 08:52:13 -0300

    computing Hurst coefficient

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from hurst import compute_Hc
+
+df = pd.read_csv('PETR4.SA.csv')    # read database containing closing price
+
+hurst, c, data = compute_Hc(df['Close'], kind='price', simplified=True) # compute Hurst coefficient
+
+f, ax = plt.subplots()
+ax.plot(data[0], c*data[0]**hurst, color="deepskyblue")
+ax.scatter(data[0], data[1], color="purple")
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('Time interval')
+ax.set_ylabel('R/S ratio')
+ax.grid(True)
+plt.show()
+
+print("H={:.4f}, c={:.4f}".format(hurst, c))
